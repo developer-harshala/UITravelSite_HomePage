3a513a2b2dd0e6bfda8289cdbca2c9850d1e8ce8
Harshala Gawli <rinkusgawli@gmail.com>
2024-05-06 03:09:47 +0530

    header changes

--- Components/Header.js
+++ Components/Header.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import Navbar from './Navbar'
+import styles from '../public/CSS/styles.module.css'
 
 const Header = () => {
   return (
-    <div className='container' style={{ padding: '12px' }}>
+    <div
+      className={`container ${styles.fadeInAnimation}`}
+      style={{ padding: '12px' }}
+    >
       <div className='row align-items-center'>
-        <div className='col-lg-2 col-md-3 col-sm-4 col-6'>
+        <div
+          className={`col-lg-2 col-md-3 col-sm-4 col-6 ${styles.fadeInAnimation}`}
+        >
           <img
             src='/images/Frame 1.png'
             alt='Logo'
+            className={styles.scaleButtonAnimation}
             style={{ padding: '10px', maxWidth: '100%' }}
           />
         </div>
-        <div className='col-lg-6 col-md-5 col-sm-4 col-6'>
+        <div
+          className={`col-lg-6 col-md-5 col-sm-4 col-6 ${styles.fadeInAnimation}`}
+        >
           <Navbar />
         </div>
-        <div className='col-lg-4 col-md-4 col-sm-4 col-12 d-flex justify-content-between mt-2'>
+        <div
+          className={`col-lg-4 col-md-4 col-sm-4 col-12 d-flex justify-content-between mt-2 ${styles.fadeInAnimation}`}
+        >
           <button
-            className='btn'
+            className={`btn ${styles.scaleButtonAnimation}`}
             style={{
               width: '45%',
               height: '100%',
@@ -30,7 +41,9 @@ const Header = () => {
           >
             Log in
           </button>
-          <button className='violetBtn'>Sign Up</button>
+          <button className={`violetBtn ${styles.scaleButtonAnimation}`}>
+            Sign Up
+          </button>
         </div>
       </div>
     </div>
